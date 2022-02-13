260d16516c1ed47c7dc894c6da0cca81dddf3c11
Raul Ryan <ryanraul_11@hotmail.com>
2022-02-13 20:16:49 -0300

    Implementação dos comentarios nos animais e Resolver para a listagem de animais
    - Criação de um modulo compartilhavel

--- gatitobook/src/app/animais/animais.module.ts
+++ gatitobook/src/app/animais/animais.module.ts
@@ -7,14 +7,19 @@ import { AnimalComponent } from './animal/animal.component';
 import { GradeFotosAnimaisComponent } from './grade-fotos-animais/grade-fotos-animais.component';
 import { CartaoModule } from '../componentes/cartao/cartao.module';
 import { DetalheAnimalComponent } from './detalhe-animal/detalhe-animal.component';
+import { ComentariosComponent } from './detalhe-animal/comentarios/comentarios.component';
+import { MensagemModule } from '../componentes/mensagem/mensagem.module';
+import { ReactiveFormsModule } from '@angular/forms';
+import { SharedModule } from '../shared/shared.module';
 
 
 @NgModule({
-  declarations: [ListaAnimaisComponent, AnimalComponent, GradeFotosAnimaisComponent, DetalheAnimalComponent],
+  declarations: [ListaAnimaisComponent, AnimalComponent, GradeFotosAnimaisComponent, DetalheAnimalComponent, ComentariosComponent],
   imports: [
     CommonModule,
     AnimaisRoutingModule,
-    CartaoModule
+    CartaoModule,
+    SharedModule
   ]
 })
 export class AnimaisModule { }
---
+++ gatitobook/src/app/animais/detalhe-animal/comentarios/comentarios.component.ts
@@ -0,0 +1,44 @@
+import { Component, Input, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { Observable } from 'rxjs';
+import { switchMap, tap } from 'rxjs/operators';
+import { Comentarios } from './comentarios';
+import { ComentariosService } from './comentarios.service';
+
+@Component({
+  selector: 'app-comentarios',
+  templateUrl: './comentarios.component.html',
+  styleUrls: ['./comentarios.component.css']
+})
+export class ComentariosComponent implements OnInit {
+
+  @Input() id!: number;
+  comentarios$!: Observable<Comentarios>;
+  comentarioForm!: FormGroup;
+  
+  constructor(
+    private comentarioService: ComentariosService, 
+    private formBuilder: FormBuilder
+  ) { }
+
+  ngOnInit(): void {
+    this.comentarios$ = this.comentarioService.buscaComentario(this.id);
+    this.comentarioForm = this.formBuilder.group({
+      comentario: ['', Validators.maxLength(300)],
+    });
+  }
+
+  gravar(): void{
+    const comentario = this.comentarioForm.get('comentario')?.value ?? '';
+    this.comentarios$ = this.comentarioService.incluiComentario(
+      this.id, 
+      comentario
+    ).pipe(
+        switchMap(() => this.comentarioService.buscaComentario(this.id)),
+        tap(() => {
+          this.comentarioForm.reset();
+          alert("Comentário Salvo!");
+        })
+    )
+  }
+}
